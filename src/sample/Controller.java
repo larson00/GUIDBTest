8f30361fa45b91ed1347fb2f78d15cbe04f888ff
larson00 <larson05@gmail.com>
2018-12-12 14:18:14 -0500

    12/12

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,74 +1,140 @@
 package sample;
 /**
- * @author Larson
- * Controller Class. OnAction contains all functionality for the GUI.
- * Connection is made with database "tester" and data is displayed using Labels.
- * resultSet stores the data from the SQL table.
- * Data was gathered from the Madden NFL 2019 team rankings.
- * 11/19/2018
+ * @author Larson Controller Class. OnAction contains all functionality for the GUI. Connection is
+ * made with database "tester" and data is displayed using Labels. resultSet stores the data from
+ * the SQL table. Data was gathered from the Madden NFL 2019 team rankings. 11/19/2018
  */
-import java.sql.Connection;
-import java.sql.DriverManager;
+
+
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.SQLSyntaxErrorException;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.ListView;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.layout.AnchorPane;
+import javafx.util.Callback;
 
 public class Controller {
 
-  @FXML
-  private Label lblTeam;
+  // Observable list for teamList
+  private ObservableList<TeamInfo> teamList = FXCollections.observableArrayList();
+
 
   @FXML
-  private Label lblDivision;
+  private AnchorPane anchor;
 
   @FXML
-  private Label lblOverall;
+  private ComboBox<TeamInfo> comboBox;
 
   @FXML
-  private Label lblOffensive;
+  private TableColumn<TeamInfo, String> teamColumn;
 
   @FXML
-  private Label lblDefensive;
+  private TableColumn<TeamInfo, String> divisionColumn;
 
   @FXML
-  private Label lblStarter;
+  private TableColumn<TeamInfo, String> overallColumn;
+
+  @FXML
+  private TableColumn<TeamInfo, String> offenseColumn;
+
+  @FXML
+  private TableColumn<TeamInfo, String> defenseColumn;
+
+  @FXML
+  private TableColumn<TeamInfo, String> starterColumn;
+
+  @FXML
+  private Button viewTeam;
+
+  @FXML
+  private Button updateRank;
+
+  @FXML
+  private TableView<TeamInfo> teamTable;
+
+  @FXML
+  private ComboBox<?> comboTest;
+
+  @FXML
+  void updateRank(ActionEvent event) {
+    //fillTable();
+    init();
+  }
 
   @FXML
   void viewTeam(ActionEvent event) {
-    // Connect to the database
-    final String DATABASE_URL = "jdbc:derby:lib\\tester";
-    final String SELECT_QUERY =
-        "SELECT name, division, overallRank, offensiveRank, defensiveRank, starterRank FROM teams";
+    // Does nothing currently
+  }
 
-    // use try-with-resources to connect to and query the database
-    try (
-        Connection connection = DriverManager.getConnection(
-            DATABASE_URL, "", "");
-        Statement statement = connection.createStatement();
-        ResultSet resultSet = statement.executeQuery(SELECT_QUERY)) {
-      // get ResultSet's meta data
-      {
-        //ResultSet object.
-        resultSet.next();
-        lblTeam.setText(resultSet.getString(1));
-        lblDivision.setText(resultSet.getString(2));
-        lblOverall.setText(resultSet.getString(3));
-        lblOffensive.setText(resultSet.getString(4));
-        lblDefensive.setText(resultSet.getString(5));
-        lblStarter.setText(resultSet.getString(6));
+  @FXML
+  void selectTeam(ActionEvent event) {
+
+    //fillTable();
+    //init();
+  }
+
+  @FXML
+  public void init() {
+    // Puts the values of teamName into the combobox
+
+    String statement = "SELECT * FROM teams";
+
+    try {
+      ResultSet resultSet = DBUtil.dbExecuteQuery(statement);
+      while (resultSet.next()) {
+        TeamInfo teamInfo = new TeamInfo();
+        teamInfo.setTeamName(resultSet.getString("name"));
+        teamInfo.setTeamDivision(resultSet.getString("division"));
+        teamInfo.setOverallRank(resultSet.getString("overallRank"));
+        teamInfo.setOffensiveRank(resultSet.getString("offensiveRank"));
+        teamInfo.setDefensiveRank(resultSet.getString("defensiveRank"));
+        teamInfo.setStarterRank(resultSet.getString("starterRank"));
+        teamList.add(teamInfo);
 
 
       }
-    } // AutoCloseable objects' close methods are called now
-    catch (SQLException sqlException) {
-      sqlException.printStackTrace();
-    } catch (Exception ex) {
+    } catch (SQLException | ClassNotFoundException e) {
       System.out.println("Error");
     }
+    // Get a list view for the combobox
+    // from https://docs.oracle.com/javafx/2/ui_controls/list-view.htm
+    Callback<ListView<TeamInfo>, ListCell<TeamInfo>> names = n -> new ListCell<TeamInfo>() {
+      @Override
+      protected void updateItem(TeamInfo item, boolean empty) {
+        super.updateItem(item, empty);
+        setText(empty ? "" : item.getTeamName());
+      }
+    };
+    // Combobox stuff
+    comboBox.setCellFactory(names);
+    comboBox.setButtonCell(names.call(null));
+    comboBox.setItems(teamList);
 
   }
 
+  private void fillTable() {
+    // Get data to the table - not working
+    teamColumn.setCellValueFactory(cellData -> cellData.getValue().teamNameProperty());
+    divisionColumn.setCellValueFactory(cellData -> cellData.getValue().teamDivisionProperty());
+    overallColumn.setCellValueFactory(cellData -> cellData.getValue().overallRankProperty());
+    offenseColumn.setCellValueFactory(cellData -> cellData.getValue().offensiveRankProperty());
+    defenseColumn.setCellValueFactory(cellData -> cellData.getValue().defensiveRankProperty());
+    starterColumn.setCellValueFactory(cellData -> cellData.getValue().starterRankProperty());
+
+    ObservableList<TeamInfo> allTeams = FXCollections.observableArrayList();
+
+
+    teamTable.setItems(allTeams);
+
+  }
 }
+
